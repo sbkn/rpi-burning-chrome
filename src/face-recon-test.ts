9c0dcd7405c8a7d42d91131112e1579f857d2b65
dkx0q1t <sergey.bakin@vwfs.com>
2019-06-03 18:42:04 +0200

    add test code for motion detection, add example video

--- src/face-recon-test.ts
+++ src/face-recon-test.ts
@@ -2,7 +2,7 @@ import * as cv from "opencv4nodejs";
 import {Vec3, Point2} from "opencv4nodejs";
 
 const INPUT_IMAGE_PATH = "./crowd-img.jpeg";
-const RESULT_IMAGE_PATH = "./result.jpeg";
+// const RESULT_IMAGE_PATH = "./result.jpeg";
 
 const BOUNDING_BOX_COLOR = new Vec3(66, 244, 110);
 
@@ -16,14 +16,18 @@ const index = async (): Promise<void> => {
 	const res = await classifier.detectMultiScaleAsync(grayImg);
 	const {objects, numDetections} = res;
 
-	console.log("objects", JSON.stringify(objects));
+	console.log(`Found ${objects.length} objects`);
 	console.log("numDetections", JSON.stringify(numDetections));
 
 	objects.forEach(object => {
 		img.drawRectangle(new Point2(object.x, object.y), new Point2(object.x + object.width, object.y + object.height), BOUNDING_BOX_COLOR);
 	});
 
-	await cv.imwriteAsync(RESULT_IMAGE_PATH, img);
+	cv.imshow('result-img', img);
+	cv.waitKey();
+	cv.destroyAllWindows();
+	// You could also save the image frame:
+	// await cv.imwriteAsync(RESULT_IMAGE_PATH, img);
 };
 
 index();
---
+++ src/motion-detect-test.ts
@@ -0,0 +1,72 @@
+import * as cv from 'opencv4nodejs';
+
+
+function grabFrames (videoFile: any, delay: number, onFrame: (frame: cv.Mat) => void): void {
+	const cap = new cv.VideoCapture(videoFile);
+	let done = false;
+	const intvl = setInterval(() => {
+		let frame = cap.read();
+		// loop back to start on end of stream reached
+		if (frame.empty) {
+			cap.reset();
+			frame = cap.read();
+		}
+		onFrame(frame);
+
+		const key = cv.waitKey(delay);
+		done = key !== -1 && key !== 255;
+		if (done) {
+			clearInterval(intvl);
+			console.log('Key pressed, exiting.');
+		}
+	}, 0);
+}
+
+function drawRectAroundBlobs (binaryImg: cv.Mat, dstImg: cv.Mat, minPxSize: number, fixedRectWidth?: number): void {
+	const {
+		centroids,
+		stats
+	} = binaryImg.connectedComponentsWithStats();
+
+	// pretend label 0 is background
+	for (let label = 1; label < centroids.rows; label += 1) {
+		const [x1, y1] = [stats.at(label, cv.CC_STAT_LEFT), stats.at(label, cv.CC_STAT_TOP)];
+		const [x2, y2] = [
+			x1 + (fixedRectWidth || stats.at(label, cv.CC_STAT_WIDTH)),
+			y1 + (fixedRectWidth || stats.at(label, cv.CC_STAT_HEIGHT))
+		];
+		const size = stats.at(label, cv.CC_STAT_AREA);
+		const blue = new cv.Vec3(255, 0, 0);
+		if (minPxSize < size) {
+			dstImg.drawRectangle(
+				new cv.Point2(x1, y1),
+				new cv.Point2(x2, y2),
+				blue,
+				2
+			);
+		}
+	}
+}
+
+const bgSubtractor = new cv.BackgroundSubtractorMOG2();
+
+const delay = 20;
+grabFrames('./traffic.mp4', delay, (frame: cv.Mat) => {
+	const foreGroundMask = bgSubtractor.apply(frame);
+
+	const iterations = 2;
+	const dilated = foreGroundMask.dilate(
+		cv.getStructuringElement(cv.MORPH_ELLIPSE, new cv.Size(4, 4)),
+		new cv.Point2(-1, -1),
+		iterations
+	);
+	const blurred = dilated.blur(new cv.Size(10, 10));
+	const thresholded = blurred.threshold(200, 255, cv.THRESH_BINARY);
+
+	const minPxSize = 4000;
+	drawRectAroundBlobs(thresholded, frame, minPxSize);
+
+	cv.imshow('foreGroundMask', foreGroundMask);
+	cv.imshow('thresholded', thresholded);
+	cv.imshow('frame', frame);
+});
