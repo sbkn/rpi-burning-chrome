5ac8679dda00c57e02fd1aacd0213fbf54ced576
dkx0q1t <sergey.bakin@vwfs.com>
2019-06-03 16:33:03 +0200

    add test code, add test img, add typescript

---
+++ src/ts-test.ts
@@ -0,0 +1,29 @@
+import * as cv from "opencv4nodejs";
+import {Vec3, Point2} from "opencv4nodejs";
+
+const INPUT_IMAGE_PATH = "./crowd-img.jpeg";
+const RESULT_IMAGE_PATH = "./result.jpeg";
+
+const BOUNDING_BOX_COLOR = new Vec3(66, 244, 110);
+
+const classifier = new cv.CascadeClassifier(cv.HAAR_FRONTALFACE_ALT2);
+
+const index = async (): Promise<void> => {
+
+	const img = await cv.imreadAsync(INPUT_IMAGE_PATH);
+
+	const grayImg = await img.bgrToGrayAsync();
+	const res = await classifier.detectMultiScaleAsync(grayImg);
+	const {objects, numDetections} = res;
+
+	console.log("objects", JSON.stringify(objects));
+	console.log("numDetections", JSON.stringify(numDetections));
+
+	objects.forEach(object => {
+		img.drawRectangle(new Point2(object.x, object.y), new Point2(object.x + object.width, object.y + object.height), BOUNDING_BOX_COLOR);
+	});
+
+	await cv.imwriteAsync(RESULT_IMAGE_PATH, img);
+};
+
+index();
